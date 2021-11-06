1ee1b2dd9cab108ca250a8da5e4ccd6eb6868c3a
Dmytro Tereshchenko <legat4life@gmail.com>
2021-11-06 17:42:50 +0200

    add web api REST for Car and Manufacturer

---
+++ CarShop.WebApi/Controllers/CarController.cs
@@ -0,0 +1,119 @@
+﻿using CarShop.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarController : ControllerBase
+    {
+        private readonly CarShopContext context;
+        public CarController(CarShopContext context)
+        {
+            this.context = context;
+        }
+        //api/car
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> Get()
+        {
+            return await context.Cars.Include(context => context.Manufacturer).ToListAsync();
+        }
+
+        //api/car/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> Get(int id)
+        {
+            try
+            {
+                Car car = await context.Cars.Include(context => context.Manufacturer).FirstAsync(c => c.Id == id);
+                if (car == null)
+                    return NotFound();
+                return Ok(car);
+            }
+            catch (ArgumentNullException)
+            {
+                return BadRequest();
+            }
+        }
+
+        //api/car
+        [HttpPost]
+        public async Task<ActionResult<Car>> Post([FromBody] Car car)
+        {
+            if (car == null)
+            {
+                return BadRequest();
+            }
+            if (car.Manufacturer != null)
+            {
+                car.ManufacturerId = car.Manufacturer.Id;
+                car.Manufacturer = null;
+            }
+            context.Cars.Add(car);
+            await context.SaveChangesAsync();
+            return Ok(car);
+        }
+
+        //api/car
+        [HttpPut]
+        public async Task<ActionResult<Car>> Put([FromBody] Car car)
+        {
+            if (car == null)
+            {
+                return BadRequest();
+            }
+            if (!context.Cars.Any(c => c.Id == car.Id))
+                return NotFound();
+            if (car.Manufacturer != null)
+            {
+                car.ManufacturerId = car.Manufacturer.Id;
+                car.Manufacturer = null;
+            }
+            context.Cars.Update(car);
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(car.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok(car);
+        }
+        //api/car/2
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Car car = await context.Cars.FirstAsync(t => t.Id == id);
+                if (car == null)
+                    return NotFound();
+                context.Cars.Remove(car);
+                await context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (ArgumentNullException)
+            {
+                return BadRequest();
+            }
+        }
+        private bool CarExists(int id)
+        {
+            return context.Cars.Any(c => c.Id == id);
+        }
+    }
+}
---
+++ CarShop.WebApi/Controllers/ManufacturerController.cs
@@ -0,0 +1,111 @@
+﻿using CarShop.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarShop.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManufacturerController : ControllerBase
+    {
+        private readonly CarShopContext context;
+        public ManufacturerController(CarShopContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<ManufacturerController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Manufacturer>>> Get()
+        {
+            return await context.Manufactures.ToListAsync();
+        }
+
+        // GET api/<ManufacturerController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Manufacturer>> Get(int id)
+        {
+            try
+            {
+                Manufacturer manufacturer = await context.Manufactures.FirstAsync(m => m.Id == id);
+                if (manufacturer == null)
+                    return NotFound();
+                return Ok(manufacturer);
+            }
+            catch (ArgumentNullException)
+            {
+                return BadRequest();
+            }
+        }
+
+        // POST api/<ManufacturerController>
+        [HttpPost]
+        public async Task<ActionResult<Manufacturer>> Post([FromBody] Manufacturer manufacturer)
+        {
+            if (manufacturer == null)
+            {
+                return BadRequest();
+            }
+            context.Manufactures.Add(manufacturer);
+            await context.SaveChangesAsync();
+            return Ok(manufacturer);
+        }
+
+        // PUT api/<ManufacturerController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Manufacturer>> Put([FromBody] Manufacturer manufacturer)
+        {
+            if (manufacturer == null)
+            {
+                return BadRequest();
+            }
+            if (!context.Manufactures.Any(m => m.Id == manufacturer.Id))
+                return NotFound();
+            context.Manufactures.Update(manufacturer);
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ManufacturerExists(manufacturer.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok(manufacturer);
+        }
+
+        // DELETE api/<ManufacturerController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Manufacturer manufacturer = await context.Manufactures.FirstAsync(m => m.Id == id);
+                if (manufacturer == null)
+                    return NotFound();
+                context.Manufactures.Remove(manufacturer);
+                await context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (ArgumentNullException)
+            {
+                return BadRequest();
+            }
+        }
+        private bool ManufacturerExists(int id)
+        {
+            return context.Manufactures.Any(c => c.Id == id);
+        }
+    }
+}
